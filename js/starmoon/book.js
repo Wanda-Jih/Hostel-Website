bb14dd9fe2b3820e67d49b1e5b012f74432d5eed
Wanda-Jih <43124480+Wanda-Jih@users.noreply.github.com>
2019-12-03 20:25:08 +0800

    upload the project

---
+++ js/starmoon/book.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+
+});
+
+function DeleteOrder() {
+    var r = confirm("確定要刪除此項");
+    if (r == true) {
+        alert("刪除成功");
+    }
+}
+
+function RefreshBook() {
+    var r = confirm("系統將無法保留先前資料\n若要保留資料，請按取消");
+    if (r == true) {
+        window.location.reload();
+    }
+}
---
+++ js/starmoon/traffic.js
@@ -0,0 +1,24 @@
+﻿
+	
+$(document).ready(function(){
+	$('#airplane').on('click',function(){
+		$('#BB1').attr("src","images/airplane.png");
+		// $(".img-map").css("padding","40px!;");
+			
+	});
+		
+});
+$(document).ready(function(){
+    $('#boat').on('click',function(){
+        $('#BB1').attr("src","images/boat.png");
+			
+    });
+		
+});
+$(document).ready(function(){
+    $('#map').on('click',function(){
+        $('#BB1').attr("src","https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.702211077123!2d119.51292310044505!3d23.615010494575206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb4f6fdc950a390b1!2z5aSn5rGg5pif5pyI54Gj5rCR5a6_!5e0!3m2!1szh-TW!2stw!4v1543848348616");
+			
+    });
+		
+});
